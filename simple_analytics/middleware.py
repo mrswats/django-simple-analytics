0d6fe429a1c1fba8eee582f5d2bac4e6920f66c5
Jernej Rus <jernej.rus@abelium.com>
2024-11-13 17:11:20 +0100

    Remove unused imports

--- simple_analytics/middleware.py
+++ simple_analytics/middleware.py
@@ -4,7 +4,7 @@ from typing import Any, Callable
 from urllib.parse import urlparse, urlunparse
 
 from django.core.exceptions import MultipleObjectsReturned
-from django.db.models import F, Sum
+from django.db.models import Sum
 from django.http import HttpRequest, HttpResponse
 
 from simple_analytics.models import VisitPerPage
